73c5562312bfa834b5621d887fb12d53612bde2e
Addhanki Manikanta <99521159+maniaddhanki@users.noreply.github.com>
2022-05-18 11:08:05 +0530

    added mineField.sh to run the game
    added gameDisplay.js to show the status

---
+++ gameDisplay.js
@@ -0,0 +1,8 @@
+const { getData } = require('./initLib.js');
+const gameData = getData('./gameData.json');
+
+const createTable = ({ field }) => field.reverse().map(row => row.join(' | ')).join('\n');
+
+const pointLocation = gameData => createTable(gameData).replace(gameData.position, '📍');
+
+console.log(pointLocation(gameData));
--- mineField.sh
+++ mineField.sh
@@ -1,14 +1,14 @@
 read -p "select starting column from 1,2,3 : " position
 
-$(node gameInit.js $position)
-stat=$(cat gameData.json | grep "status" | cut -f4 -d\")
-# cat gameData.json
-echo $stat
-# while (($status == 'continue'))
-# do
-#   read direction
-#   $(node mineField.js ${position} ${direction})
-#   status=$(cat gameData.json | grep "status" | cut -f4 -d\")
-#   cat gameData.json
-# done
-
+node gameInit.js $position
+cat gameData.json | grep '"status":"continue"' > /dev/null
+stat=$?
+node gameDisplay.js
+while (( $stat == 0 ))
+do
+  read direction
+  node mineField.js ${direction}
+  cat gameData.json | grep '"status":"continue"' > /dev/null
+  stat=$?
+  node gameDisplay.js
+done
